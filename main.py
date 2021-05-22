2d94f904cd78eaf6d0b44edbd631405d7b628782
Nekhil Nagia <nekhilnagia16@gmail.com>
2021-05-21 20:59:55 -0400

    formatting

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from tqdm import trange
 from model import lstm_seq2seq
 import numpy as np
 
-if torch.cuda.is_available():  
-  device = "cuda:0" 
-else:  
-  device = "cpu" 
+if torch.cuda.is_available():
+    device = "cuda:0"
+else:
+    device = "cpu"
 
 torch.set_printoptions(precision=10)
 torch.set_printoptions(edgeitems=100)
@@ -19,8 +19,10 @@ torch.set_printoptions(edgeitems=100)
 print(f"Running on {device}")
 
 # Simple assembly processor
+
+
 def execute_assembly(instructions):
-    registers = [0,0,0,0,0,0] # R0, R1, R2, R3, R4, R5
+    registers = [0, 0, 0, 0, 0, 0]  # R0, R1, R2, R3, R4, R5
     for instruction in instructions:
         instr_type = instruction.split(" ")[0]
         source = instruction.split(" ")[1]
@@ -30,7 +32,7 @@ def execute_assembly(instructions):
             source = registers[int(source[-1])]
 
         dest_reg = int(instruction.split(" ")[2][-1])
-        
+
         if instr_type == "ADD":
             registers[dest_reg] += int(source)
         elif instr_type == "SUB":
@@ -40,16 +42,21 @@ def execute_assembly(instructions):
     return registers
 
 # Generate a random assembly instruction
+
+
 def generate_assembly_instruction():
     instr = random.choice(["ADD", "SUB", "MOV"])
     value = random.randint(10, 99)
-    destination_reg = "R" + str(random.randint(1,5))
-    source_reg = "R" + str(random.randint(0,5))
-    
+    destination_reg = "R" + str(random.randint(1, 5))
+    source_reg = "R" + str(random.randint(0, 5))
+
     source_reg_or_val = random.choice([str(value), source_reg])
     return instr + " " + source_reg_or_val + " " + destination_reg
 
-all_chars = ["A", "D", "S", "U", "B", "M", "O", "V", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "R", " ", "~"]
+
+all_chars = ["A", "D", "S", "U", "B", "M", "O", "V", "1", "2",
+             "3", "4", "5", "6", "7", "8", "9", "0", "R", " ", "~"]
+
 
 def s_to_i(s):
     instr_num = []
@@ -57,10 +64,12 @@ def s_to_i(s):
         instr_num.append(all_chars.index(char))
     return instr_num
 
+
 def create_dataset(num_samples=10000):
-    x,y = [], []
+    x, y = [], []
     for _ in trange(num_samples):
-        instructions = [generate_assembly_instruction() for _ in range(random.randint(1,10))]
+        instructions = [generate_assembly_instruction()
+                        for _ in range(random.randint(1, 10))]
 
         result = execute_assembly(instructions)
         result = torch.tensor(result, dtype=torch.float)
@@ -76,6 +85,7 @@ def create_dataset(num_samples=10000):
     y = torch.stack(y)
     return x, y
 
+
 train_x, train_y = create_dataset(num_samples=40000)
 val_x, val_y = create_dataset(num_samples=1000)
 
@@ -85,7 +95,7 @@ validation_dataset = TensorDataset(val_x, val_y)
 model = lstm_seq2seq(21, 128)
 model = model.to(device)
 training_loss, validation_loss = model.train_model(train_dataset=train_dataset, batch_size=64, n_epochs=5, target_len=6, validation_dataset=validation_dataset,
- training_prediction="recursive")
+                                                   training_prediction="recursive")
 
 plt.plot(training_loss)
 plt.plot(validation_loss)
--- model.py
+++ model.py
@@ -9,34 +9,33 @@ from torch import optim
 import torch.nn.functional as F
 from torch.utils.data.dataloader import DataLoader
 
-if torch.cuda.is_available():  
-    device = "cuda:0" 
-else:  
-    device = "cpu" 
+if torch.cuda.is_available():
+    device = "cuda:0"
+else:
+    device = "cpu"
+
 
 class lstm_encoder(nn.Module):
     ''' Encodes time-series sequence '''
 
-    def __init__(self, input_size, hidden_size, num_layers = 1):
-        
+    def __init__(self, input_size, hidden_size, num_layers=1):
         '''
         : param input_size:     the number of features in the input X
         : param hidden_size:    the number of features in the hidden state h
         : param num_layers:     number of recurrent layers (i.e., 2 means there are
         :                       2 stacked LSTMs)
         '''
-        
+
         super(lstm_encoder, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
         # define LSTM layer
-        self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size,
-                            num_layers = num_layers, batch_first = True)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True)
 
     def forward(self, x_input):
-        
         '''
         : param x_input:               input of shape (seq_len, # in batch, input_size)
         : return lstm_out, hidden:     lstm_out gives all the hidden states in the sequence;
@@ -45,63 +44,61 @@ class lstm_encoder(nn.Module):
         '''
 
         lstm_out, self.hidden = self.lstm(x_input)
-        
-        return lstm_out, self.hidden     
-    
+
+        return lstm_out, self.hidden
+
     def init_hidden(self, batch_size):
-        
         '''
         initialize hidden state
         : param batch_size:    x_input.shape[1]
         : return:              zeroed hidden state and cell state 
         '''
-        
+
         return (torch.zeros(batch_size, self.num_layers, self.hidden_size).to(device),
                 torch.zeros(batch_size, self.num_layers, batch_size, self.hidden_size).to(device))
 
 
 class lstm_decoder(nn.Module):
     ''' Decodes hidden state output by encoder '''
-    
-    def __init__(self, input_size, hidden_size, num_layers = 1):
 
+    def __init__(self, input_size, hidden_size, num_layers=1):
         '''
         : param input_size:     the number of features in the input X
         : param hidden_size:    the number of features in the hidden state h
         : param num_layers:     number of recurrent layers (i.e., 2 means there are
         :                       2 stacked LSTMs)
         '''
-        
+
         super(lstm_decoder, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        self.lstm = nn.LSTM(input_size = input_size, hidden_size = hidden_size,
-                            num_layers = num_layers)
-        self.linear = nn.Linear(hidden_size, input_size)           
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers)
+        self.linear = nn.Linear(hidden_size, input_size)
 
     def forward(self, x_input, encoder_hidden_states):
-        
         '''        
         : param x_input:                    should be 2D (batch_size, input_size)
         : param encoder_hidden_states:      hidden states
         : return output, hidden:            output gives all the hidden states in the sequence;
         :                                   hidden gives the hidden state and cell state for the last
         :                                   element in the sequence 
- 
+
         '''
-        
-        lstm_out, self.hidden = self.lstm(x_input.unsqueeze(0), encoder_hidden_states)
-        output = self.linear(lstm_out.squeeze(0))     
-        
+
+        lstm_out, self.hidden = self.lstm(
+            x_input.unsqueeze(0), encoder_hidden_states)
+        output = self.linear(lstm_out.squeeze(0))
+
         return output, self.hidden
+
 
 class lstm_seq2seq(nn.Module):
     ''' train LSTM encoder-decoder and make predictions '''
-    
-    def __init__(self, input_size, hidden_size):
 
+    def __init__(self, input_size, hidden_size):
         '''
         : param input_size:     the number of expected features in the input X
         : param hidden_size:    the number of features in the hidden state h
@@ -112,46 +109,27 @@ class lstm_seq2seq(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        self.encoder = lstm_encoder(input_size = input_size, hidden_size = hidden_size)
-        self.decoder = lstm_decoder(input_size = 1, hidden_size = hidden_size)
+        self.encoder = lstm_encoder(
+            input_size=input_size, hidden_size=hidden_size)
+        self.decoder = lstm_decoder(input_size=1, hidden_size=hidden_size)
 
+    def train_model(self, train_dataset, batch_size, n_epochs, target_len, validation_dataset, training_prediction='recursive',
+                    teacher_forcing_ratio=0.5, learning_rate=0.01, dynamic_tf=False):
 
-    def train_model(self, train_dataset, batch_size, n_epochs, target_len, validation_dataset, training_prediction = 'recursive', teacher_forcing_ratio = 0.5, learning_rate = 0.01, dynamic_tf = False):
-        
-        '''
-        train lstm encoder-decoder
-        
-        : param input_tensor:              input data with shape (batch_size, seq_len, number features); PyTorch tensor    
-        : param target_tensor:             target data with shape (batch_size, seq_len, number features); PyTorch tensor
-        : param n_epochs:                  number of epochs 
-        : param target_len:                number of values to predict 
-        : param batch_size:                number of samples per gradient update
-        : param training_prediction:       type of prediction to make during training ('recursive', 'teacher_forcing', or
-        :                                  'mixed_teacher_forcing'); default is 'recursive'
-        : param teacher_forcing_ratio:     float [0, 1) indicating how much teacher forcing to use when
-        :                                  training_prediction = 'teacher_forcing.' For each batch in training, we generate a random
-        :                                  number. If the random number is less than teacher_forcing_ratio, we use teacher forcing.
-        :                                  Otherwise, we predict recursively. If teacher_forcing_ratio = 1, we train only using
-        :                                  teacher forcing.
-        : param learning_rate:             float >= 0; learning rate
-        : param dynamic_tf:                use dynamic teacher forcing (True/False); dynamic teacher forcing
-        :                                  reduces the amount of teacher forcing for each epoch
-        : return losses:                   array of loss function for each epoch
-        '''
-    
-        # initialize array of losses 
+        # initialize array of losses
         losses = np.full(n_epochs, np.nan)
         val_losses = []
 
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
+        train_loader = DataLoader(
+            train_dataset, batch_size=batch_size, drop_last=True)
 
-        optimizer = optim.Adam(self.parameters(), lr = learning_rate)
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
         criterion = nn.L1Loss(reduction="sum")
 
         with trange(n_epochs) as tr:
             batches = 0
             for it in tr:
-                
+
                 batch_loss = 0.
                 batch_loss_tf = 0.
                 batch_loss_no_tf = 0.
@@ -159,7 +137,7 @@ class lstm_seq2seq(nn.Module):
                 num_no_tf = 0
 
                 for i, data in enumerate(train_loader):
-                    # select data 
+                    # select data
                     code, target = data
                     code = code.float()
 
@@ -184,70 +162,76 @@ class lstm_seq2seq(nn.Module):
 
                     if training_prediction == 'recursive':
                         # predict recursively
-                        for t in range(target_len): 
-                            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                        for t in range(target_len):
+                            decoder_output, decoder_hidden = self.decoder(
+                                decoder_input, decoder_hidden)
                             outputs[t] = decoder_output
                             decoder_input = decoder_output
 
                     if training_prediction == 'teacher_forcing':
                         # use teacher forcing
                         if random.random() < teacher_forcing_ratio:
-                            for t in range(target_len): 
-                                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                            for t in range(target_len):
+                                decoder_output, decoder_hidden = self.decoder(
+                                    decoder_input, decoder_hidden)
                                 outputs[t] = decoder_output
-                                decoder_input = (target[:,t]).unsqueeze(-1)
+                                decoder_input = (target[:, t]).unsqueeze(-1)
 
-                        # predict recursively 
+                        # predict recursively
                         else:
-                            for t in range(target_len): 
-                                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                            for t in range(target_len):
+                                decoder_output, decoder_hidden = self.decoder(
+                                    decoder_input, decoder_hidden)
                                 outputs[t] = decoder_output
                                 decoder_input = decoder_output
 
                     if training_prediction == 'mixed_teacher_forcing':
                         # predict using mixed teacher forcing
                         for t in range(target_len):
-                            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                            decoder_output, decoder_hidden = self.decoder(
+                                decoder_input, decoder_hidden)
                             outputs[t] = decoder_output
-                            
+
                             # predict with teacher forcing
                             if random.random() < teacher_forcing_ratio:
-                                decoder_input = (target[:,t]).unsqueeze(-1)
-                            
-                            # predict recursively 
+                                decoder_input = (target[:, t]).unsqueeze(-1)
+
+                            # predict recursively
                             else:
                                 decoder_input = decoder_output
                     batches += 1
 
-                    outputs = torch.transpose(outputs.squeeze(), 0, 1).to(device)
+                    outputs = torch.transpose(
+                        outputs.squeeze(), 0, 1).to(device)
 
-                    # compute the loss 
+                    # compute the loss
                     loss = criterion(outputs, target)
 
                     batch_loss += loss.item()
-                    
+
                     # backpropagation
                     loss.backward()
                     optimizer.step()
 
-                # loss for epoch 
+                # loss for epoch
                 batch_loss /= batches
                 # losses[it] = batch_loss
-                losses[it] = self.calculate_loss(train_dataset, 6)[1] # TEMP DELETE LATER
+                losses[it] = self.calculate_loss(train_dataset, 6)[
+                    1]  # TEMP DELETE LATER
 
                 # dynamic teacher forcing
                 if dynamic_tf and teacher_forcing_ratio > 0:
-                    teacher_forcing_ratio = teacher_forcing_ratio - 0.02 
+                    teacher_forcing_ratio = teacher_forcing_ratio - 0.02
 
-                # progress bar 
+                # progress bar
                 tr.set_postfix(loss="{0:.3f}".format(batch_loss))
-                val_losses.append(self.calculate_loss(validation_dataset, 6)[1])
-                
+                val_losses.append(self.calculate_loss(
+                    validation_dataset, 6)[1])
+
         return losses, val_losses
 
     @torch.no_grad()
     def calculate_loss(self, dataset, target_len):
-        
         '''
         : param input_tensor:      input data (seq_len, input_size); PyTorch tensor 
         : param target_len:        number of target values to predict 
@@ -270,19 +254,20 @@ class lstm_seq2seq(nn.Module):
 
             # initialize tensor for predictions
             outputs = torch.zeros(target_len, batch_size, 1)
-            
+
             # initialize hidden state
             encoder_output, encoder_hidden = self.encoder(input_tensor)
 
             # decode input_tensor
             decoder_input = torch.zeros(batch_size, 1).to(device)
             decoder_hidden = encoder_hidden
-            
+
             for t in range(target_len):
-                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                decoder_output, decoder_hidden = self.decoder(
+                    decoder_input, decoder_hidden)
                 outputs[t] = decoder_output
                 decoder_input = decoder_output
-            
+
             outputs = torch.transpose(outputs.squeeze(), 0, 1).to(device)
             outputs_np = outputs.cpu().numpy()
             target_np = target_tensor.numpy()
@@ -291,5 +276,5 @@ class lstm_seq2seq(nn.Module):
 
         loss /= batches
         result = torch.stack(results)
-        
+
         return result, loss
